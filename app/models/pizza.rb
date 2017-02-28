57fd1ba1b123316f88c80d07aa480c66a0fc4f5c
Miłosz Sędziak <m.sedziak01@gmail.com>
2017-02-28 21:42:02 +0100

    Add validation to model pizza

--- app/models/pizza.rb
+++ app/models/pizza.rb
@@ -1,3 +1,6 @@
 class Pizza < ApplicationRecord
   belongs_to :restaurant
+
+  validates :name, :price, :ingredients, :restaurant_id, presence: true
+  validates_numericality_of :price, greater_than_or_equal_to: 0, less_than: 100000
 end
