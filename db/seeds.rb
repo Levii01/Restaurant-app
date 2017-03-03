7623ab62bda6587ddb34feea436d0744e0b76be5
Miłosz Sędziak <m.sedziak01@gmail.com>
2017-03-03 22:42:02 +0100

    Add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,27 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+puts 'Seeds: start'
+OPENING = %w( 08:00 08:30 09:00 09:30 10:00)
+CLOSING = %w( 17:00 18:00 20:00 21:00)
+RESTAURANT_NAME = %w(Hoth Kashyyyk Yavin Kamino Endor Corellia Devaron)
+RESTAURANT_NAME.each do |name|
+  Restaurant.create!(
+  name: name,
+  address: Faker::Address.street_address ,
+  opening_time: OPENING.sample,
+  closing_time: CLOSING.sample,
+  opening_day: Restaurant::DAYS.first,
+  closing_day: Restaurant::DAYS[rand(4)+3]
+  )
+end
+
+Restaurant.all.each do |restaurant|
+  9.times do |i|
+    restaurant.pizzas.create!(
+    name: Pizza::PIZZA_TYPE[i],
+    price: Faker::Number.decimal(2),
+    ingredients: [Faker::Food.ingredient, Faker::Food.ingredient, Faker::Food.ingredient].join(', ')
+    ) if rand(2).zero?
+  end
+end
+
+puts 'Seeds: finish'
